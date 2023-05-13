ce06a63683def8e8e9a7023a645fb17318ff5a5d
Srinivas <sm1043@gmail.com>
2023-05-13 12:38:03 +0530

    changed code

--- PdfToExcel/__init__.py
+++ PdfToExcel/__init__.py
@@ -32,15 +32,6 @@ def main(myblob: func.InputStream):
     # Get a BlobClient object for the PDF file
     blob_client = blob_service_client.get_blob_client(container_name, blob_name)
 
-    # Download the PDF file as a stream
-    stream = io.BytesIO()
-    #blob_client.download_blob().download_to_stream(stream)
-
-    # Use the stream to read the PDF file
-    # For example, you could use the PyPDF2 library to extract text from the PDF file
-    import PyPDF2
-    pdf_reader = PyPDF2.PdfFileReader(stream)
-    page = pdf_reader.getPage(0)
-    text = page.extractText()
-    logging.info(f"PDF text: {text}")
+    logging.info(blob_client, blob_name)
+        
     
